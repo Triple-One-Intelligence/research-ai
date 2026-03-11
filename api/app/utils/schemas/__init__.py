967c0030472d5179487b47b40f47ede8001c8858
Sybren <s.c.m.vandermeijden@students.uu.nl>
2026-03-11 16:40:45 +0100

    Revert "Fixes missing __init__.py in schemas folder"
    This reverts commit 8b1d313b9fdbaf8d1919887bfd0371a2ae1ba82d.

--- api/app/utils/schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-from .connections import Connections, Member
-from .organization import Organization
-from .person import Person
-from .publication import Publication
-from .suggestions import Suggestions
-
-__all__ = ["Person", "Publication", "Organization", "Suggestions", "Connections", "Member"]
